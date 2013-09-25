efa90eceea5f06a2950c924e200ede0309c2276b
shmuro <sh.muro@gmail.com>
2013-09-25 16:55:14 +0300

    renamed package

--- client/src/com/rb/server/ServerUtilities.java
+++ client/src/com/rb/server/ServerUtilities.java
@@ -1,23 +1,8 @@
-/*
- * Copyright 2012 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.google.android.gcm.demo.app;
+package com.rb.server;
 
-import static com.google.android.gcm.demo.app.CommonUtilities.SERVER_URL;
-import static com.google.android.gcm.demo.app.CommonUtilities.TAG;
-import static com.google.android.gcm.demo.app.CommonUtilities.displayMessage;
+import static com.rb.server.CommonUtilities.SERVER_URL;
+import static com.rb.server.CommonUtilities.TAG;
+import static com.rb.server.CommonUtilities.displayMessage;
 
 import java.io.IOException;
 import java.io.OutputStream;
